4031f5fe8a43e39e3d9839197308302924add0a3
rightson <chari.rightson@gmail.com>
2024-01-13 23:51:31 +0300

    Fix conditional rendering of Projects component

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -10,7 +10,7 @@ export default async function About() {
     justify-between bg-white"
     >
       <Hero />
-      <Projects projects={projects} />
+      {projects && <Projects projects={projects} />}
     </main>
   );
 }
